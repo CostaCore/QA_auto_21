6bf19397d5a7492506e452735b041122236aa5fb
CostaCore <nsczxfxthntq@gmail.com>
2021-05-20 09:03:09 +0300

    20-06

---
+++ README.md
@@ -0,0 +1,2 @@
+# QA_auto_2021
+QA avtomation course 18-05-2021
---
+++ src/main/java/hw_1/task_1.java
@@ -0,0 +1,11 @@
+package hw_1;
+
+public class task_1 {
+    public static void main(String[] args) {
+        String text = "JAVA HOME";
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(text+i);
+        }
+    }
+}
